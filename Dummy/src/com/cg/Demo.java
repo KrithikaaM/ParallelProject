ecdb73bef9726d2a7c86e07310ce7f3874b7692d
kmurugai <kmurugai@DIN66009004.corp.capgemini.com>
2018-07-06 17:32:12 +0530

    this is a dummy code

---
+++ Dummy/src/com/cg/Demo.java
@@ -0,0 +1,10 @@
+package com.cg;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Dummy prog");
+		System.out.println("The end!!!!!!!!");
+	}
+}
